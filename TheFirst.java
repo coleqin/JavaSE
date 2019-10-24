841880a9629b18caaf6861d56976f55f002ab757
coleqin <44968017+coleqin@users.noreply.github.com>
2019-10-24 22:14:20 +0800

    Create TheFirst.java

---
+++ TheFirst.java
@@ -0,0 +1,30 @@
+
+public class ConstantAndVar{
+	
+	static int d ;
+	public static void main(String[] args){
+		//声明
+		int a;
+		//赋值
+		a =10;
+		//声明+赋值
+		//int b = 20;
+		//int c;
+		//System.out.println(d);
+		//int x=10,y=20;
+		
+		System.out.println(a);
+		a=30;
+		System.out.println(a);
+		final int c = 20;
+		System.out.println(c);
+		c=30;
+		System.out.println(c);
+
+
+
+		
+		
+	}
+	
+}
